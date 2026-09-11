35acf79ec9711a6d870715e05f840a90a9103f27
wangboxuan49-rgb <wangboxuan49@gmail.com>
2026-09-11 15:12:19 +0800

    ch03:完成第一部分练习题

---
+++ ch03/print_char.c
@@ -0,0 +1,14 @@
+/* 功能:打印字符
+   作者:wbx
+   日期:2026-09-11 */
+
+#include<stdio.h>
+int main(void){
+   int asc2;
+
+   printf("请输入一个\" ASC2 \"码值");
+   scanf("%d",&asc2);
+   printf("对应的\" ASC2 \"码值为 %c \n",asc2);
+
+   return 0;
+}
