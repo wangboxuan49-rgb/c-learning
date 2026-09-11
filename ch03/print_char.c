b5e307f5919ed6911bdec6f7e4af8a637ca78b10
wangboxuan49-rgb <wangboxuan49@gmail.com>
2026-09-11 15:17:33 +0800

    ch03:添加习题中注释

--- ch03/print_char.c
+++ ch03/print_char.c
@@ -6,9 +6,9 @@
 int main(void){
    int asc2;
 
-   printf("请输入一个\" ASC2 \"码值");
+   printf("请输入一个\" ASC2 \"码值");    //利用转义序列来实现打印出双引号
    scanf("%d",&asc2);
-   printf("对应的\" ASC2 \"码值为 %c \n",asc2);
+   printf("对应的\" ASC2 \"码值为 %c \n",asc2);    //利用 ASC2 码，特定的整数表示特定的字符
 
    return 0;
 }
