1f936aa406cd7b4d64b1611b2e213840f9119312
wangboxuan49-rgb <wangboxuan49@gmail.com>
2026-09-13 14:44:52 +0800

    check:完成第一周验收

---
+++ check/week_1.c
@@ -0,0 +1,14 @@
+/* 功能:第一周验收
+   作者:wbx
+   日期:2026-09-13 */
+
+#include <stdio.h>      //include 后加空格
+
+int main(void){
+   int i_num = 3;    //声明变量
+   double d_dec = 4.123456;
+
+   printf("%d %.2f\n",i_num,d_dec);    //打印变量，保留两位小数，不要忘记换行
+
+   return 0;
+}
